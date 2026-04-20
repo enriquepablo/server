2f5371ee2997f24a9abd7bd80744c2a08032509a
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-04-20 13:04:32 +0200

    fix[dav]: composer autoload
    Signed-off-by: Enrique Pérez Arnaud <enrique@cazalla.net>

--- apps/dav/composer/composer/autoload_static.php
+++ apps/dav/composer/composer/autoload_static.php
@@ -415,6 +415,7 @@ class ComposerStaticInitDAV
         'OCA\\DAV\\Migration\\Version1038Date20260302000000' => __DIR__ . '/..' . '/../lib/Migration/Version1038Date20260302000000.php',
         'OCA\\DAV\\Migration\\Version1039Date20260408000000' => __DIR__ . '/..' . '/../lib/Migration/Version1039Date20260408000000.php',
         'OCA\\DAV\\Migration\\Version1037Date20260306120000' => __DIR__ . '/..' . '/../lib/Migration/Version1037Date20260306120000.php',
+        'OCA\\DAV\\Migration\\Version1038Date20260302000000' => __DIR__ . '/..' . '/../lib/Migration/Version1038Date20260302000000.php',
         'OCA\\DAV\\Model\\ExampleEvent' => __DIR__ . '/..' . '/../lib/Model/ExampleEvent.php',
         'OCA\\DAV\\Paginate\\LimitedCopyIterator' => __DIR__ . '/..' . '/../lib/Paginate/LimitedCopyIterator.php',
         'OCA\\DAV\\Paginate\\PaginateCache' => __DIR__ . '/..' . '/../lib/Paginate/PaginateCache.php',
